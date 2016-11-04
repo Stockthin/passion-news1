fd9aa0a169f567d79c32b8c69526b15181dfce16
Tran Thi Minh Tam <minhtam.tran1795@gmail.com>
2016-11-04 10:42:44 +0700

    giao diện register

--- app/app-routes.js
+++ app/app-routes.js
@@ -1,18 +1,22 @@
 function config($stateProvider, $urlRouterProvider) {
-    
+
     $urlRouterProvider.otherwise("/home");
     $stateProvider
         .state('home', {
             url: '/home',
             templateUrl: 'app/components/home/home.html',
             controller: 'homeCtrl as home',
-            data: { pageTitle: 'HOME' }
+            data: {
+                pageTitle: 'HOME'
+            }
         })
         .state('detail', {
             url: '/detail/:id',
             templateUrl: 'app/components/detail/detail.html',
             controller: 'detailCtrl as detail',
-            data: { pageTitle: 'DETAIL' }
+            data: {
+                pageTitle: 'DETAIL'
+            }
         })
         // .state('header', {
         //     url: '/header',
@@ -33,7 +37,7 @@ function config($stateProvider, $urlRouterProvider) {
         .state('user_profile', {
             url: '/user_profile',
             views: {
-                '' : {
+                '': {
                     templateUrl: 'app/components/user_profile/user_profile.html'
                 },
                 // 'view@user_profile' : {
@@ -42,7 +46,7 @@ function config($stateProvider, $urlRouterProvider) {
                 // 'edit@user_profile' : {
                 //      templateUrl: 'app/components/user_profile/edit_account/edit_account.html'
                 // },
-                'avatar_custom@user_profile' : {
+                'avatar_custom@user_profile': {
                     templateUrl: 'app/components/user_profile/avatar_custom/avatar_custom.html'
                 }
             }
@@ -52,64 +56,75 @@ function config($stateProvider, $urlRouterProvider) {
             url: '/view_profiles',
             templateUrl: 'app/components/user_profile/view_profiles/view_profiles.html',
             // controller: 'abouUsCtrl as aboutUs',
-            
+
         })
         .state('user_profile.edit', {
             url: '/edit',
             templateUrl: 'app/components/user_profile/edit_account/edit_account.html',
             // controller: 'abouUsCtrl as aboutUs',
-            
+
         })
         .state('user_profile.change_pass', {
             url: '/change_pass',
             templateUrl: 'app/components/user_profile/change_password/change_password.html',
             // controller: 'abouUsCtrl as aboutUs',
-            
+
         })
         .state('user_profile.post', {
             url: '/post_news',
             templateUrl: 'app/components/user_profile/post_news/post_news.html',
             // controller: 'abouUsCtrl as aboutUs',
-            
+
         })
         .state('about_us', {
             url: '/about_us',
             templateUrl: 'app/components/about_us/about_us.html',
             controller: 'aboutUsCtrl as aboutUs',
-            data: { pageTitle: 'About_US' }
+            data: {
+                pageTitle: 'About_US'
+            }
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'app/components/register/register.html',
+            controller: 'registerCtrl as register',
+            data: {
+                pageTitle: 'register'
+            }
         })
         .state('detail_categories', {
             url: '/detail_categories/:id',
             templateUrl: 'app/components/detail_categories/detail_categories.html',
             controller: 'categoriesCtrl as categories',
-            data: { pageTitle: 'DETAIL' }
+            data: {
+                pageTitle: 'DETAIL'
+            }
         })
 
-        //Login
-        // .state('login', {
-        //     url: '/login',
-        //     templateUrl: 'view/login.html',
-        //     controller: 'LoginCtrl as login',
-        //     data: { pageTitle: 'LOGIN' }
-        // })
-        // .state('register', {
-        //     url: '/register',
-        //     templateUrl: 'view/register.html',
-        //     controller: 'RegisterCtrl as register',
-        //     data: { pageTitle: 'REGISTER' }
-        // })
-        // .state('userProfile', {
-        //     url: '/user-profile',
-        //     templateUrl: 'view/user-profile.html',
-        //     controller: 'UserProCtrl as userProfile',
-        //     data: { pageTitle: 'USER-PROFILE' }
-        // })
-        // .state('forgot-pass', {
-        //     url: "/forgot-pass",
-        //     templateUrl: "view/forgot-pass.html",
-        //     controller: 'ForgotPassCtrl as forgot',
-        //     data: { pageTitle: 'FORGOT' }
-        // });
+    //Login
+    // .state('login', {
+    //     url: '/login',
+    //     templateUrl: 'view/login.html',
+    //     controller: 'LoginCtrl as login',
+    //     data: { pageTitle: 'LOGIN' }
+    // })
+    // .state('register', {
+    //     url: '/register',
+    //     templateUrl: 'view/register.html',
+    //     controller: 'RegisterCtrl as register',
+    //     data: { pageTitle: 'REGISTER' }
+    // })
+    // .state('userProfile', {
+    //     url: '/user-profile',
+    //     templateUrl: 'view/user-profile.html',
+    //     controller: 'UserProCtrl as userProfile',
+    //     data: { pageTitle: 'USER-PROFILE' }
+    // })
+    // .state('forgot-pass', {
+    //     url: "/forgot-pass",
+    //     templateUrl: "view/forgot-pass.html",
+    //     controller: 'ForgotPassCtrl as forgot',
+    //     data: { pageTitle: 'FORGOT' }
+    // });
 }
 app.config(config);
-
--- app/controller.js
+++ app/controller.js
@@ -1,200 +1,200 @@
+function mainCtrl($scope, $http) {
+    // $http
+    // 	.get('api/news.json')
+    // 	.then(function(respone){
+    // 		var news = respone.data;
+    // 		$scope.news = news;
+    // 		console.log($scope.news);
+    // 	});
 
-function mainCtrl($scope, $http){
-	// $http
-	// 	.get('api/news.json')
-	// 	.then(function(respone){
-	// 		var news = respone.data;
-	// 		$scope.news = news;
-	// 		console.log($scope.news);
-	// 	});
-	
-	// $scope.url = 'assets/img/'; 
-	
+    // $scope.url = 'assets/img/'; 
+
 }
 
-function homeCtrl($scope, $http){
-	//Lấy top news
-	var topnews = [];
-  	$scope.topnews = topnews;
-  	$http
-		.get("api/news.json")
-		.then(function(respone){
-			var news = respone.data;
-			news.reverse();
-			//Top 1 new
-			$scope.top1new = news[0];
-			
-			//Duyệt mảng news + Lấy top 4 tin mới nhất
-			news.forEach(function(item, key){
-				var tintuc = item;
-				var key = key;
-				//Lấy 4 tin mới nhất
-				if(key < 5 && key > 0){
-				    topnews.push(tintuc);
-				}
-		});
-	//Lấy Categories
-	// var categories = [];
-	// var top_news_with_categories = [];
- //  	$http
-	// 	.get('api/Category.json')
-	// 	.then(function(respone){
-	// 		categories = respone.data;
-	// 		$scope.categories = respone.data;
-	// 		//Duyệt mảng Categories
-	// 		categories.forEach(function(item,key) {
-	// 			var id_category = item.IDCategory;
-				
-	// 		});
-	// 	});
-					
-	});
-	//Lấy tin theo categories
-	var news_other_by_cat = [];
-	$http
-		.get('api/news_by_categories.json')
-		.then(function(respone){
-			var news_by_categories = respone.data;
-			$scope.news_by_categories = news_by_categories;
-			news_by_categories.forEach(function(item, key) {
-				var news_by_category = item;
-				var id_news_by_cat = item.IDCategory;
-				news_by_category.news.forEach(function(item2, key2) {
-					if(id_news_by_cat == item2.IDCategory){
-						var new_other_by_cat = item2;
-						news_other_by_cat.push(new_other_by_cat);
-						$scope.news_other_by_cat = news_other_by_cat;
+function homeCtrl($scope, $http) {
+    //Lấy top news
+    var topnews = [];
+    $scope.topnews = topnews;
+    $http
+        .get("api/news.json")
+        .then(function (respone) {
+            var news = respone.data;
+            news.reverse();
+            //Top 1 new
+            $scope.top1new = news[0];
 
-					}
-					// console.log(news_other_by_cat);
-				});
-				// console.log(item);
-			});
-			// console.log(news_by_categories);
-		});
+            //Duyệt mảng news + Lấy top 4 tin mới nhất
+            news.forEach(function (item, key) {
+                var tintuc = item;
+                var key = key;
+                //Lấy 4 tin mới nhất
+                if (key < 5 && key > 0) {
+                    topnews.push(tintuc);
+                }
+            });
+            //Lấy Categories
+            // var categories = [];
+            // var top_news_with_categories = [];
+            //  	$http
+            // 	.get('api/Category.json')
+            // 	.then(function(respone){
+            // 		categories = respone.data;
+            // 		$scope.categories = respone.data;
+            // 		//Duyệt mảng Categories
+            // 		categories.forEach(function(item,key) {
+            // 			var id_category = item.IDCategory;
+
+            // 		});
+            // 	});
+
+        });
+    //Lấy tin theo categories
+    var news_other_by_cat = [];
+    $http
+        .get('api/news_by_categories.json')
+        .then(function (respone) {
+            var news_by_categories = respone.data;
+            $scope.news_by_categories = news_by_categories;
+            news_by_categories.forEach(function (item, key) {
+                var news_by_category = item;
+                var id_news_by_cat = item.IDCategory;
+                news_by_category.news.forEach(function (item2, key2) {
+                    if (id_news_by_cat == item2.IDCategory) {
+                        var new_other_by_cat = item2;
+                        news_other_by_cat.push(new_other_by_cat);
+                        $scope.news_other_by_cat = news_other_by_cat;
+
+                    }
+                    // console.log(news_other_by_cat);
+                });
+                // console.log(item);
+            });
+            // console.log(news_by_categories);
+        });
 
 }
 //Detail News
-function detailCtrl($scope, $http, $stateParams){
-	var id = $stateParams.id;
-	var other_news =[];
-	var hotnews = [];
-	// console.log(id);
-	$http
-		.get('api/news.json')
-		.then(function(respone){
-			var news = respone.data;
-			news.reverse();
-			news.forEach(function(item,key){
-				//Show tin tức được chọn
-				if(id == item.IDNews) {
-					$scope.new = item;
-					$scope.id_category = item.IDCategory;
-					
-				}
-				//Lấy tin tức cùng categories
-				if($scope.id_category == item.IDCategory) {
-					
-					$scope.id2 = item.IDNews;
-					var other_new = item;
-					other_news.push(other_new);
-					$scope.other_news = other_news;
-					// console.log($scope.other_news);
-					
-				}	
-				//Hot news
-				var hotnew = item;
-				if(key < 5){
-					hotnews.push(hotnew);
-					$scope.hotnews = hotnews;
-					console.log(hotnews);
-				}
-				//Lấy tên danh mục tin của tin được chọn
-				$http
-					.get('api/Category.json')
-					.then(function(respone){
-						var categories = respone.data;
-						categories.forEach(function(item,key) {
-							if($scope.id_category == item.IDCategory){
-								// console.log(item.NameCategory);
-								$scope.name_category = item.NameCategory;
-							}
-							
-						});
-					});
+function detailCtrl($scope, $http, $stateParams) {
+    var id = $stateParams.id;
+    var other_news = [];
+    var hotnews = [];
+    // console.log(id);
+    $http
+        .get('api/news.json')
+        .then(function (respone) {
+            var news = respone.data;
+            news.reverse();
+            news.forEach(function (item, key) {
+                //Show tin tức được chọn
+                if (id == item.IDNews) {
+                    $scope.new = item;
+                    $scope.id_category = item.IDCategory;
 
-				
-			});
-			
-		});
-	
+                }
+                //Lấy tin tức cùng categories
+                if ($scope.id_category == item.IDCategory) {
+
+                    $scope.id2 = item.IDNews;
+                    var other_new = item;
+                    other_news.push(other_new);
+                    $scope.other_news = other_news;
+                    // console.log($scope.other_news);
+
+                }
+                //Hot news
+                var hotnew = item;
+                if (key < 5) {
+                    hotnews.push(hotnew);
+                    $scope.hotnews = hotnews;
+                    console.log(hotnews);
+                }
+                //Lấy tên danh mục tin của tin được chọn
+                $http
+                    .get('api/Category.json')
+                    .then(function (respone) {
+                        var categories = respone.data;
+                        categories.forEach(function (item, key) {
+                            if ($scope.id_category == item.IDCategory) {
+                                // console.log(item.NameCategory);
+                                $scope.name_category = item.NameCategory;
+                            }
+
+                        });
+                    });
+
+
+            });
+
+        });
+
 }
 
 //Detail Categories
-function categoriesCtrl($scope, $http, $stateParams){
-	var id = $stateParams.id;
-	
-	// console.log(id);
-	$http
-		.get('api/news_by_categories.json')
-		.then(function(respone){
-			var news_by_categories = respone.data;
-			news_by_categories.reverse();
-			// console.log(news);
-			news_by_categories.forEach(function(item,key){
-				//Show tin tức được chọn
-				if(id == item.IDCategory) {
-					var news_by_category = item.news;
-					$scope.news_by_category = news_by_category;
-					console.log(news_by_category);
-					// $scope.new = item;
-					// $scope.id_category = item.IDCategory;
-					
-				}
-				//Lấy tin tức cùng categories
-				// if($scope.id_category == item.IDCategory) {
-					
-				// 	$scope.id2 = item.IDNews;
-				// 	var other_new = item;
-				// 	other_news.push(other_new);
-				// 	$scope.other_news = other_news;
-				// 	// console.log($scope.other_news);
-					
-				// }	
-			});
-		})
-	
+function categoriesCtrl($scope, $http, $stateParams) {
+    var id = $stateParams.id;
+
+    // console.log(id);
+    $http
+        .get('api/news_by_categories.json')
+        .then(function (respone) {
+            var news_by_categories = respone.data;
+            news_by_categories.reverse();
+            // console.log(news);
+            news_by_categories.forEach(function (item, key) {
+                //Show tin tức được chọn
+                if (id == item.IDCategory) {
+                    var news_by_category = item.news;
+                    $scope.news_by_category = news_by_category;
+                    console.log(news_by_category);
+                    // $scope.new = item;
+                    // $scope.id_category = item.IDCategory;
+
+                }
+                //Lấy tin tức cùng categories
+                // if($scope.id_category == item.IDCategory) {
+
+                // 	$scope.id2 = item.IDNews;
+                // 	var other_new = item;
+                // 	other_news.push(other_new);
+                // 	$scope.other_news = other_news;
+                // 	// console.log($scope.other_news);
+
+                // }	
+            });
+        })
+
 }
 
-function headerCtrl($scope, $http){
-	
+function headerCtrl($scope, $http) {
+
 }
-function footerCtrl(){
-  
+
+function footerCtrl() {
+
 }
 //Main Menu
-function mainMenuCtrl($scope, $http){
-  $http
-		.get('api/Category.json')
-		.then(function(respone){
-			$scope.categories = respone.data;
-		});
-	
+function mainMenuCtrl($scope, $http) {
+    $http
+        .get('api/Category.json')
+        .then(function (respone) {
+            $scope.categories = respone.data;
+        });
+
 }
 
 //User profile
-function userProCtrl(){
+function userProCtrl() {
 
 }
 
 //About us
-function aboutUsCtrl(){
-	
+function aboutUsCtrl() {
+
 }
 
 //View Profiles 
-function viewProCtrl(){
-	
+function viewProCtrl() {
+
 }
 app.controller('mainCtrl', mainCtrl);
 app.controller('viewProCtrl', viewProCtrl);
@@ -206,3 +206,4 @@ app.controller('detailCtrl', detailCtrl);
 app.controller('headerCtrl', headerCtrl);
 app.controller('footerCtrl', footerCtrl);
 app.controller('mainMenuCtrl', mainMenuCtrl);
+app.controller('aboutUsCtrl', registerCtrl);
